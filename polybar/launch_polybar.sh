33c3b2e94e4d74a3f095f13921266764e1e32aa3
Griffith000 <latrechghassen68@gmail.com>
2025-11-22 23:07:08 +0100

    add polybar

---
+++ polybar/launch_polybar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+killall polybar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload doig &
+  done
+else
+  polybar --reload doig &
+fi
